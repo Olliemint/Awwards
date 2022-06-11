b37e85a425f9aea250b70aca915183581c9f7d24
Olliemint <furywrld5@gmail.com>
2022-06-11 16:14:26 +0300

    feat:created project model

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Projectdata(models.Model):
+    
+    name = models.CharField(max_length=200, blank=False)
+    posted_date = models.DateTimeField(auto_now_add=True)
+    posted_by = models.CharField(max_length=200, blank=False)
+    location = models.CharField(max_length=200, blank=False)
+    project_link = models.URLField(max_length=500, blank=True)
--- project_awwards/settings.py
+++ project_awwards/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awwards.apps.AwwardsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
