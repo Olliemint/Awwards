183163ebc0ca3471a4ff7337ed2f0de7043693aa
Olliemint <furywrld5@gmail.com>
2022-06-12 01:36:38 +0300

    feat: add login register and logout functionality

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('register/', views.Register, name='register'),
     path('login/',views.Login,name='login'),
     path('logout/',views.Logout,name='logout'),
     
--- awwards/views.py
+++ awwards/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from .forms import RegisterUserForm
+from django.contrib import messages
+
+
+from django.shortcuts import render,redirect
 from rest_framework import viewsets
 from .serializers import ProjectSerializer
 from .models import Projectdata
 import requests
+
 
 # Create your views here.
 class ProjectView(viewsets.ModelViewSet):
@@ -22,12 +28,48 @@ def home(request):
     return render(request, 'awwards/home.html',context)
 
 
+def Register(request):
+    
+    
+    form = RegisterUserForm()
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully.')
+            return redirect('login')
+        
+        
+    
+    
+    
+    context = {
+        'form': form,
+    }
+    
+    return render(request, 'awwards/register.html',context)
+
+
 
 def Login(request):
+    if request.method == 'POST':
+        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            
+            return redirect('home')
+        else:
+            messages.warning(request, 'Username or password is incorrect')
     
     return render(request, 'awwards/login.html')
 
 
 def Logout(request):
     
-    return render(request, 'awwards/logout.html')
+    logout(request)
+    
+    return redirect('home')
