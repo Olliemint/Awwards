7ee7cbab6c0a2866d1ac9ccad651b907cf359573
Olliemint <furywrld5@gmail.com>
2022-06-11 17:29:12 +0300

    feat: serialized Project model

--- awwards/serializers.py
+++ awwards/serializers.py
@@ -1,1 +1,12 @@
-from 
+from pyexpat import model
+from rest_framework import serializers
+from .models import Projectdata
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Projectdata
+        
+        fields = ['name', 'posted_date', 'posted_by', 'location' ,'project_link']
+
