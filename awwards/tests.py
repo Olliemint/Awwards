8a1a56b7e2d7883d6f6e9e56fd23523efeb0c1c7
Olliemint <furywrld5@gmail.com>
2022-06-14 11:03:41 +0300

    feat: added review tests

--- awwards/tests.py
+++ awwards/tests.py
@@ -3,6 +3,23 @@ from .models import Profile, Projectdata,Review,password
 from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestReview(TestCase):
+    def setUp(self):
+        self.project = Projectdata(author= User.objects.create(username='Olliemint'),name='Akan Names',  location='Nairobi', project_link='http://akanify.herokuapp.com', project_image='test.jpg',)
+        self.user = User( username='Olliemint', password='@test.password')
+        self.rating = Review.objects.create(design=6, usability=7, content=9, user=self.user)
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Review))
+
+    def test_save_rating(self):
+        self.rating.save_rating()
+        rating = Review.objects.all()
+        self.assertTrue(len(rating) > 0)
+
+
+
+
 
 class TestProfile(TestCase):
     
@@ -18,7 +35,7 @@ class TestProfile(TestCase):
 
 class TestProject(TestCase):
     def setUp(self):
-        self.project = Projectdata(author= User.objects.create(username='Olliemint'),name='Akan Names', image='test.jpg',  description='a clone for delani studio', livelink='http://google.com/?search=this+is+amazing')
+        self.project = Projectdata(author= User.objects.create(username='Olliemint'),name='Akan Names',  location='Nairobi', project_link='http://akanify.herokuapp.com', project_image='test.jpg',)
         self.user = User(username='Olliemint', password='@test.password')
 
     def test_instance(self):
@@ -31,24 +48,11 @@ class TestProject(TestCase):
 
     def test_get_project(self):
         self.project.save()
-        posts = Projectdata.posts()
-        self.assertTrue(len(posts) > 0)
+        project = Projectdata.project()
+        self.assertTrue(len(project) > 0)
 
     def test_search_post(self):
         self.project.save()
         project = Projectdata.search_projects('test')
         self.assertFalse(len(project) > 0)
 
-class TestRate(TestCase):
-    def setUp(self):
-        self.post = Projectdata(title='Delani Studion', image='test.jpg', author= User.objects.create(username='mercy'), description='a clone for delani studio', livelink='http://google.com/?search=this+is+amazing')
-        self.user = User(id=1, username='mercy', password='pre123456')
-        self.rating = Review.objects.create(id=1, design=6, usability=7, content=9, user=self.user)
-    
-    def test_instance(self):
-        self.assertTrue(isinstance(self.rating, Review))
-
-    def test_save_rating(self):
-        self.rating.save_rating()
-        rating = Review.objects.all()
-        self.assertTrue(len(rating) > 0)
